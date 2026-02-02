1c03920e316c6677a041d4e9450435d40c044827
simon lehericey <mail@simon.lehericey.net>
2026-02-02 14:29:34 +0100

    refactor: prepare ocr_service

--- app/services/ocr_service.rb
+++ app/services/ocr_service.rb
@@ -1,30 +1,39 @@
 # frozen_string_literal: true
 
 class OCRService
+  include Dry::Monads[:result]
+  extend Dry::Monads[:result]
+
   def self.analyze(blob)
-    url = ENV.fetch("OCR_SERVICE_URL", nil)
-
-    return Dry::Monads::Failure(retryable: false, reason: StandardError.new('OCR not configured')) if url.nil? # Service is not enabled
-
     blob_url = blob.url
-    json = { "url": blob_url, "hint": { "type": "rib" } }
-    headers = { 'X-Remote-File': blob_url } # needed for logging
-
-    handle_api_result(API::Client.new.call(url:, method: :post, headers:, json:, timeout: 31))
+    analyze_rib(blob_url)
   end
 
   private
 
-  def self.handle_api_result(result)
-    case result
-    in Dry::Monads::Success(body:)
-      # store directly the data in value_json
-      # as there is no transformation to do
-      Dry::Monads::Success(value_json: body)
-    in Dry::Monads::Failure(code:, reason:)
-      Dry::Monads::Failure(retryable: false, reason:, code:)
+  def self.analyze_rib(blob_url)
+    return not_configured("OCR_SERVICE_URL") if ocr_url.nil?
+
+    json = { "url": blob_url, "hint": { "type": "rib" } }
+    headers = { 'X-Remote-File': blob_url } # needed for logging
+
+    API::Client.new.call(url: ocr_url, method: :post, headers:, json:, timeout: 31)
+      .fmap { |ok| { value_json: ok.body } } # store directly in value_json without transformation
+      .or { to_retryable_failure(it) }
+  end
+
+  def self.ocr_url = ENV.fetch("OCR_SERVICE_URL", nil)
+
+  def self.not_configured(message)
+    Failure(retryable: false, reason: StandardError.new("#{message} not configured"))
+  end
+
+  def self.to_retryable_failure(data)
+    case data
+    in code:, reason:
+      Failure(retryable: false, reason:, code:)
     else
-      Dry::Monads::Failure(retryable: false, reason: StandardError.new('Unknown error'))
+      Failure(retryable: false, reason: StandardError.new('Unknown error'))
     end
   end
 end
