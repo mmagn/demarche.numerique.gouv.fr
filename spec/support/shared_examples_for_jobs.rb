46f05458293bb8f2f0d53d71164b4bbee1f88910
Mathieu Magnin <mathieu@magnin.sh>
2026-02-05 11:08:56 +0100

    refactor(tests): ensure Sentry.capture_exception is called

--- spec/support/shared_examples_for_jobs.rb
+++ spec/support/shared_examples_for_jobs.rb
@@ -8,10 +8,14 @@ RSpec.shared_examples 'a job retrying transient errors' do |job_class = describe
   end if !defined?(ExconErrorJob)
 
   context 'when a transient network error is raised' do
-    it 'makes 5 attempts before raising the exception up' do
+    it 'makes 5 attempts before discarding the job and reporting the error to Sentry' do
+      allow(Sentry).to receive(:capture_exception)
+
       assert_performed_jobs 5 do
         ExconErrorJob.perform_later rescue Excon::Error::InternalServerError
       end
+
+      expect(Sentry).to have_received(:capture_exception).with(instance_of(Excon::Error::InternalServerError), anything).exactly(5).times
     end
   end
 end
@@ -24,7 +28,7 @@ RSpec.shared_examples 'a job retrying standard errors' do |job_class = described
   end if !defined?(StandardErrorJob)
 
   context 'when another type of error is raised' do
-    it 'makes 25 attempts before raising the exception up (default strategy)' do
+    it 'makes 25 attempts before discarding the job (default strategy)' do
       assert_performed_jobs 25 do
         StandardErrorJob.perform_later rescue StandardError
       end
