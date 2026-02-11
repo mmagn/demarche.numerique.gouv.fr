1049e4e5faef459463a640889c493169049b7078
Colin Darie <colin@darie.eu>
2026-02-11 22:16:24 +0100

    refactor(auto_rotate): replace MiniMagick with ruby-vips

--- app/services/auto_rotate_service.rb
+++ app/services/auto_rotate_service.rb
@@ -1,36 +1,18 @@
 # frozen_string_literal: true
 
+require "vips"
+
 class AutoRotateService
   def process(file, output)
-    auto_rotate_image(file, output)
-  end
+    image = Vips::Image.new_from_file(file.to_path)
+    orientation = image.get("orientation")
 
-  private
+    return nil if orientation == 1 # TopLeft = no rotation needed
 
-  def auto_rotate_image(file, output)
-    image = MiniMagick::Image.new(file.to_path)
-
-    return nil if !image.valid?
-
-    case image["%[orientation]"]
-    when 'LeftBottom'
-      rotate_image(file, output, 90)
-    when 'BottomRight'
-      rotate_image(file, output, 180)
-    when 'RightTop'
-      rotate_image(file, output, 270)
-    else
-      nil
-    end
-  end
-
-  def rotate_image(file, output, degree)
-    MiniMagick.convert do |convert|
-      convert << file.to_path
-      convert.rotate(degree)
-      convert.auto_orient
-      convert << output.to_path
-    end
+    rotated = Vips::Image.new_from_file(file.to_path, autorotate: true)
+    rotated.write_to_file(output.to_path)
     output
+  rescue Vips::Error
+    nil
   end
 end
