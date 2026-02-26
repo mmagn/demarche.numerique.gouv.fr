575823d014e4c23cc39f45c356542c379111a418
Colin Darie <colin@darie.eu>
2026-02-26 17:12:18 +0100

    refactor(watermark): use vips instead of magick

--- spec/services/watermark_service_spec.rb
+++ spec/services/watermark_service_spec.rb
@@ -1,24 +1,26 @@
 # frozen_string_literal: true
 
-RSpec.describe WatermarkService do
+RSpec.describe WatermarkService, :external_deps do
   let(:image) { file_fixture("logo_test_procedure.png") }
   let(:watermark_service) { WatermarkService.new }
 
   describe '#process' do
-    it 'returns a tempfile if watermarking succeeds', :external_deps do
-      Tempfile.create do |output|
+    it 'returns a tempfile if watermarking succeeds' do
+      Tempfile.create(["watermarked", ".png"]) do |output|
         watermark_service.process(image, output)
         # output size should always be a little greater than input size
         expect(output.size).to be_between(image.size, image.size * 1.5)
       end
     end
 
-    it 'returns nil if metadata is blank' do
-      allow(watermark_service).to receive(:image_metadata).and_return(nil)
+    it 'raises Vips::Error for unsupported files' do
+      Tempfile.create(["invalid", ".txt"]) do |invalid_file|
+        invalid_file.write("not an image")
+        invalid_file.flush
 
-      Tempfile.create do |output|
-        expect(watermark_service.process(image.to_path, output)).to be_nil
-        expect(output.size).to eq(0)
+        Tempfile.create(["output", ".png"]) do |output|
+          expect { watermark_service.process(invalid_file, output) }.to raise_error(Vips::Error)
+        end
       end
     end
   end
