3624121f6ae0ddb8be52c7c1d62eb7cfdf499d3c
Colin Darie <colin@darie.eu>
2026-03-05 12:05:55 +0100

    feat(identite): don't pre-check pour vous, hide form until choice made

--- spec/views/users/dossiers/identite.html.erb_spec.rb
+++ spec/views/users/dossiers/identite.html.erb_spec.rb
@@ -18,11 +18,24 @@ describe 'users/dossiers/identite', type: :view do
       expect(rendered).to have_content "Pour une autre personne"
       expect(rendered).to have_content "Vous déposez ce dossier pour un bénéficiaire en tant que mandataire"
 
-    it 'has identity fields' do
-      within('.individual-infos') do
-        expect(rendered).to have_field(id: 'Prenom')
-        expect(rendered).to have_field(id: 'Nom')
-      end
+      # does not pre-check any radio
+      expect(rendered).not_to have_checked_field("radio-self-manage")
+      expect(rendered).not_to have_checked_field("radio-tiers-manage")
+
+      # not show identity form until a choice is made'
+      expect(rendered).not_to have_text("Votre identité")
+    end
+  end
+
+  context 'when procedure has for_tiers_enabled and identity already set' do
+    let(:procedure) { create(:simple_procedure, :for_individual) }
+    let(:dossier) { create(:dossier, :with_service, :with_individual, state: Dossier.states.fetch(:brouillon), procedure: procedure, identity_updated_at: Time.current) }
+
+    it 'shows the identity form' do
+      expect(rendered).to have_css('form#identite-form')
+
+      expect(rendered).to have_checked_field("radio-self-manage")
+      expect(rendered).not_to have_checked_field("radio-tiers-manage")
     end
 
     context 'when the demarche asks for the birthdate' do
@@ -34,10 +47,10 @@ describe 'users/dossiers/identite', type: :view do
     end
   end
 
-  context 'when procedure has for_tiers_enabled' do
+  context 'when procedure does not have for_tiers_enabled' do
     let(:procedure) { create(:simple_procedure, :for_individual, for_tiers_enabled: false) }
 
-    it 'has choice for you or a tiers' do
+    it 'does not show for_tiers choice' do
       expect(rendered).not_to have_content "Pour vous"
       expect(rendered).not_to have_content "Pour une autre personne"
     end
