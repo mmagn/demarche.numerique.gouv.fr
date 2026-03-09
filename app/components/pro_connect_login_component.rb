2d119ded874596866dac8d643b15017532e3184c
Kara Diaby <kdiaby.pro@gmail.com>
2026-03-09 09:14:05 +0100

    refactor(pro_connect): enrich ProConnectLoginComponent and remove duplicate partial

--- app/components/pro_connect_login_component.rb
+++ app/components/pro_connect_login_component.rb
@@ -1,4 +1,30 @@
 # frozen_string_literal: true
 
 class ProConnectLoginComponent < ApplicationComponent
+  def initialize(url: nil, title: :default, heading_level: :h2)
+    @url = url
+    @title = title
+    @heading_level = heading_level
+  end
+
+  def before_render
+    @url ||= helpers.pro_connect_login_path
+  end
+
+  def render?
+    ProConnectService.enabled?
+  end
+
+  private
+
+  def resolved_title
+    case @title
+    when :default
+      t('.title')
+    when nil
+      nil
+    else
+      @title
+    end
+  end
 end
