b02874d93ea6f6f2a23aa93fdc29202dfbafb1e1
Mathieu Magnin <mathieu@magnin.sh>
2026-03-12 17:49:38 +0100

    feat(ami): create notification payload

--- app/services/ami/create_notification_service.rb
+++ app/services/ami/create_notification_service.rb
@@ -2,6 +2,10 @@
 
 module Ami
   class CreateNotificationService
+    SOURCE = ApplicationHelper::APP_HOST
+
+    ITEM_TYPE = "dossier"
+
     AMI_NOTIFICATIONS_ENABLED_STATES = [
       :en_construction,
       :en_instruction,
@@ -10,6 +14,15 @@ module Ami
       :sans_suite,
       :repasser_en_instruction,
     ].freeze
+
+    ITEM_GENERIC_STATUS_BY_STATE = {
+      en_construction: "new",
+      en_instruction: "wip",
+      repasser_en_instruction: "wip",
+      accepte: "closed",
+      refuse: "closed",
+      sans_suite: "closed",
+    }.freeze
 
     attr_reader :dossier, :state
 
@@ -25,7 +38,23 @@ module Ami
     def call
       return if !eligible?
 
-      # send notification to AMI
+      Rails.logger.debug { "AMI notification eligible for dossier #{dossier.id} (state: #{state})" }
+
+      payload = create_notification_payload(send_date: Time.zone.now.iso8601)
+    end
+
+    def create_notification_payload(send_date:)
+      {
+        recipient_fc_hash: RecipientFcHash.call(dossier.user),
+        content_title: "Mise a jour du dossier #{dossier.id}",
+        content_body: "Le dossier #{dossier.id} est maintenant #{status_label}.",
+        item_type: ITEM_TYPE,
+        item_id: dossier.id.to_s,
+        item_status_label: status_label,
+        item_generic_status: item_generic_status,
+        item_canal: SOURCE,
+        send_date:,
+      }
     end
 
     private
@@ -38,6 +67,14 @@ module Ami
       return ":ami_notifications feature flag disabled" unless dossier.procedure.feature_enabled?(:ami_notifications)
 
       return "state: #{state} not eligible" unless AMI_NOTIFICATIONS_ENABLED_STATES.include?(state)
+    end
+
+    def status_label
+      I18n.t("activerecord.attributes.dossier/state.#{dossier.state}", default: dossier.state.humanize)
+    end
+
+    def item_generic_status
+      ITEM_GENERIC_STATUS_BY_STATE.fetch(state.to_sym, ITEM_GENERIC_STATUS_BY_STATE.fetch(state, "wip"))
     end
   end
 end
