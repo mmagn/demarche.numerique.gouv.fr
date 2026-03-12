1bae1c93696e5e468d0ea7b1f9d47ad151d494d4
Mathieu Magnin <mathieu@magnin.sh>
2026-03-12 17:49:12 +0100

    feat(ami): check if notification should be sent

--- app/services/ami/create_notification_service.rb
+++ app/services/ami/create_notification_service.rb
@@ -2,8 +2,20 @@
 
 module Ami
   class CreateNotificationService
+    AMI_NOTIFICATIONS_ENABLED_STATES = [
+      :en_construction,
+      :en_instruction,
+      :accepte,
+      :refuse,
+      :sans_suite,
+      :repasser_en_instruction,
+    ].freeze
+
+    attr_reader :dossier, :state
+
     def initialize(dossier:)
       @dossier = dossier
+      @state = dossier.state
     end
 
     def self.call(dossier:)
@@ -11,6 +23,21 @@ module Ami
     end
 
     def call
+      return if !eligible?
+
+      # send notification to AMI
+    end
+
+    private
+
+    def eligible?
+      not_eligible_reason.blank?
+    end
+
+    def not_eligible_reason
+      return ":ami_notifications feature flag disabled" unless dossier.procedure.feature_enabled?(:ami_notifications)
+
+      return "state: #{state} not eligible" unless AMI_NOTIFICATIONS_ENABLED_STATES.include?(state)
     end
   end
 end
