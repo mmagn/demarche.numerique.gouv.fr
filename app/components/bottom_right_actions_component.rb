178457baa73ca53dec12b073866ff38891d3696d
Mathieu Magnin <mathieu@magnin.sh>
2026-03-16 22:04:22 +0100

    feat(bottom right actions): i18n component

--- app/components/bottom_right_actions_component.rb
+++ app/components/bottom_right_actions_component.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
 
 class BottomRightActionsComponent < ApplicationComponent
+  def render?
+    render_back_to_top_button? || render_need_help_button?
+  end
+
+  def render_back_to_top_button?
+    helpers.administrateur_signed_in? || helpers.instructeur_signed_in?
+  end
+
+  def render_need_help_button?
+    ENV.enabled?("CRISP") && !helpers.chatbot_disabled_page? && helpers.user_signed_in?
+  end
 end
