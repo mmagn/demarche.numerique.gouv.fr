ab3e48b5bce3e193d83dfe2ef1629223f8ffa18f
benoitqueyron <72251526+Benoit-MINT@users.noreply.github.com>
2026-03-25 10:07:32 +0100

    chore(doc): link to the doc on demarche numerique

--- config/initializers/02_urls.rb
+++ config/initializers/02_urls.rb
@@ -27,7 +27,7 @@ WEBINAIRE_URL = "https://app.livestorm.co/demarches-simplifiees"
 INSCRIPTION_WEBINAIRE_URL = "https://bbb-dinum-scalelite.visio.education.fr/playback/presentation/2.3/cbb6e43626fa1b67755d9fb05ecf5e7f3be50d48-1675342730585"
 CALENDLY_URL = "https://calendly.com/demarches-simplifiees/accompagnement-administrateur-demarches-simplifiees-fr"
 
-DOC_URL = ENV.fetch("DOC_URL", "https://doc.demarches-simplifiees.fr")
+DOC_URL = ENV.fetch("DOC_URL", "https://doc.demarche.numerique.gouv.fr")
 DOC_NOUVEAUTES_URL = [DOC_URL, "nouveautes"].join("/")
 USAGER_TUTORIAL_URL = [DOC_URL, "tutoriels", "tutoriel-usager"].join("/")
 EXPERT_TUTORIAL_URL = [DOC_URL, "tutoriels", "tutoriel-expert-invite"].join("/")
